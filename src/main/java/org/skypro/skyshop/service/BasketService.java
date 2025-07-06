59942d81ebcbe569b27a06534826e72e952c864f
Joni0104 <joni99857622@mail.ru>
2025-07-06 12:49:03 +0300

    end hw 2

---
+++ src/main/java/org/skypro/skyshop/service/BasketService.java
@@ -0,0 +1,40 @@
+package org.skypro.skyshop.service;
+
+import org.skypro.skyshop.model.basket.BasketItem;
+import org.skypro.skyshop.model.basket.ProductBasket;
+import org.skypro.skyshop.model.basket.UserBasket;
+import org.skypro.skyshop.model.product.Product;
+import org.springframework.stereotype.Service;
+import java.util.*;
+import java.util.stream.Collectors;
+
+@Service
+public class BasketService {
+    private final ProductBasket basket;
+    private final StorageService storageService;
+
+    public BasketService(ProductBasket basket, StorageService storageService) {
+        this.basket = basket;
+        this.storageService = storageService;
+    }
+
+    public void addProductToBasket(UUID productId) {
+        storageService.getProductById(productId)
+                .orElseThrow(() -> new IllegalArgumentException("Product not found"));
+        basket.addProduct(productId);
+    }
+
+    public UserBasket getUserBasket() {
+        return new UserBasket(
+                basket.getProducts().entrySet().stream()
+                        .map(entry -> {
+                            UUID productId = entry.getKey();
+                            int quantity = entry.getValue();
+                            Product product = storageService.getProductById(productId)
+                                    .orElseThrow(() -> new IllegalStateException("Product not found: " + productId));
+                            return new BasketItem(product, quantity);
+                        })
+                        .collect(Collectors.toList())
+        );
+    }
+}
--- src/main/java/org/skypro/skyshop/service/StorageService.java
+++ src/main/java/org/skypro/skyshop/service/StorageService.java
@@ -67,4 +67,7 @@ public class StorageService {
         result.addAll(articles.values());
         return result;
     }
+    public Optional<Product> getProductById(UUID id) {
+        return Optional.ofNullable(products.get(id));
+    }
 }
